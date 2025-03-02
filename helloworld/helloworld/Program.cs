3832d5c842db9d968772ed5b44edca28bc7e7ee6
Ne4ghg4441 <spasibodom@gmail.com>
2025-03-02 21:57:31 +0300

    pervoe

---
+++ helloworld/helloworld/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace hello
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hello world");
+            Console.WriteLine("penis");
+            Console.ReadKey();
+        }
+    
+    }
+}
